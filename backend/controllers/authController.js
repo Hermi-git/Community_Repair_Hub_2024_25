ce07122a5436f514b811a2f0170d48ac127c5689
Hermi-git <andargiehermela1@gmail.com>
2025-04-03 17:19:39 +0300

    login and logout functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,23 +10,20 @@ export const signup = async (req, res) => {
         if (!name || !email || !password || !role || !region || !city) {
             return res.status(StatusCodes.BAD_REQUEST).json({ message: "All fields are required" });
         }
-
-
         if (!validRoles.includes(role)) {
             return res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid role. Must be 'Citizen' or 'Repair team'." });
         }
-
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(StatusCodes.BAD_REQUEST).json({ message: "User with this email already exists!" });
         }
-
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         const newUser = new User({
             name,
             email,
             password: hashedPassword,
+            imageUrl: req.file ? req.file.path : null, 
             role,
             region,
             city
@@ -43,4 +40,48 @@ export const signup = async (req, res) => {
     }
 };
 
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Please enter all required fields" });
+    }
+
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(StatusCodes.UNAUTHORIZED).json({
+                success: false,
+                message: `No account found for email ${email}`
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Invalid credentials" });
+        }
+
+        generateWebToken(res, user._id, user.role);
+
+        return res.status(StatusCodes.OK).json({
+            msg: "User logged in successfully",
+            username: user.name,
+            role: user.role,
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong, try again later!" });
+    }
+};
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("jwt");
+        return res.status(StatusCodes.OK).json({ msg: "User logged out successfully" });
+    } catch (error) {
+        console.error(error.message);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong, try again later!" });
+    }
+};
 
--- backend/server.js
+++ backend/server.js
@@ -10,4 +10,3 @@ connectToDb().then(()=>{
 
 
 
-
