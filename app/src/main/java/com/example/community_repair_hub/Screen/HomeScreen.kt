7e4f3bd581db09394d605b4b61434e5b4b8192d2
Henok Asaye <henokasaye77@gmail.com>
2025-04-27 00:42:58 +0300

    add the cards and the search bar for the home screen

--- app/src/main/java/com/example/community_repair_hub/Screen/HomeScreen.kt
+++ app/src/main/java/com/example/community_repair_hub/Screen/HomeScreen.kt
@@ -1,2 +1,338 @@
 package com.example.community_repair_hub.Screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.community_repair_hub.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier, navController: NavController) {
+    var searchValue by remember{
+        mutableStateOf("")
+    }
+    var SState = rememberScrollState()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Community Repair Hub",
+                        color = Color.Black
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {  }) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Menu",
+                            tint = Color.Black
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Image(
+                            painter = painterResource(id = R.drawable.img_2),
+                            contentDescription = "Profile",
+                            modifier = Modifier
+                                .size(36.dp)
+                                .clip(CircleShape)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF00C853)
+                )
+            )
+        },
+        content = { paddingValues ->
+            Column(modifier = Modifier
+                .padding(paddingValues)
+                .verticalScroll(SState)
+            ) {
+                Spacer(Modifier.height(5.dp))
+                OutlinedTextField(
+                    value = searchValue,
+                    onValueChange = { searchValue = it },
+                    label = { Text("Search here ...") },
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent, //
+                        focusedIndicatorColor = Color(0xFF7CFC00),
+                        disabledIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .fillMaxWidth()
+                )
+                Spacer(Modifier.height(10.dp))
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    ),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 8.dp
+                    ),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .height(200.dp)
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.img_3),
+                            contentDescription = "Issue Image",
+                            modifier = Modifier
+                                .height(150.dp)
+
+                                .clip(RoundedCornerShape(10.dp))
+                        )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Column(
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Category: Road",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "Location: Garment, AA",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "Issue Date: 3/17/2025",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "View Detail",
+                                fontSize = 20.sp,
+                                color = Color(0XFFE6FAEE),
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .padding(top = 8.dp)
+                                    .align(Alignment.End)
+                                    .clickable { navController.navigate("signup") },
+                            )
+                        }
+                    }
+                }
+                Spacer(Modifier.height(10.dp))
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    ),
+
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 8.dp,
+
+                    ),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .height(200.dp)
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.img_3),
+                            contentDescription = "Issue Image",
+                            modifier = Modifier
+                                .height(150.dp)
+
+                                .clip(RoundedCornerShape(10.dp))
+                        )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Column(
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Category: Road",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "Location: Garment, AA",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "Issue Date: 3/17/2025",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "View Detail",
+                                fontSize = 20.sp,
+                                color = Color(0XFFE6FAEE),
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .padding(top = 8.dp)
+                                    .align(Alignment.End)
+                                    .clickable { navController.navigate("signup") },
+                            )
+                        }
+                    }
+                }
+                Spacer(Modifier.height(10.dp))
+                Card(
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    ),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 8.dp
+                    ),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .height(200.dp)
+                ){
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.img_3),
+                            contentDescription = "Issue Image",
+                            modifier = Modifier
+                                .height(150.dp)
+
+                                .clip(RoundedCornerShape(10.dp))
+                        )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Column(
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(text = "Category: Road",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                )
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(text = "Location: Garment, AA",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                ))
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(text = "Issue Date: 3/17/2025",
+                                style = TextStyle(
+                                    fontSize = 20.sp
+                                ))
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "View Detail",
+                                fontSize = 20.sp,
+                                color = Color(0XFFE6FAEE),
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .padding(top = 8.dp)
+                                    .align(Alignment.End)
+                                    .clickable { navController.navigate("signup") },
+                            )
+                        }
+                    }
+
+                }
+                Spacer(Modifier.height(10.dp))
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .align(Alignment.End),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0XFFE6FAEE),
+                        contentColor = Color.Black
+                    )
+                ){
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+                    Text(text="Add Issue")
+                }
+            }
+        }
+    )
+}
