98f175c6ae997c394bbddc392971d6588c13bd80
Henok Asaye <henokasaye77@gmail.com>
2025-04-26 20:05:55 +0300

    add the input form fields

--- app/src/main/java/com/example/community_repair_hub/Screen/LoginScreen.kt
+++ app/src/main/java/com/example/community_repair_hub/Screen/LoginScreen.kt
@@ -1,12 +1,123 @@
 package com.example.community_repair_hub.Screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
 
-fun AuthScreen(modifier: Modifier = Modifier){
+fun LoginScreen(modifier: Modifier = Modifier, navController: NavHostController){
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember{
+        mutableStateOf("")
+    }
+    Column(
+        modifier=Modifier
+            .fillMaxWidth()
+            .padding(
+                start = 32.dp,
+                top = 52.dp,
+                end = 32.dp,
+                bottom = 52.dp
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text="Welcome back to the Community Repair Hub",
+            style= TextStyle(
+                fontSize = 40.sp,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+            )
+        )
+        Spacer(Modifier.height(20.dp))
+        Text(
+            text="Empowering Communities, One Fix at a Time!",
+            style= TextStyle(
+                fontSize = 25.sp,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+            )
+        )
+        Spacer(Modifier.height(20.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            placeholder = {Text("Email")},
+            label = {Text("Email Address")},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(25.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            placeholder = {Text("Email")},
+            label = {Text("password")},
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(12.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color(0xFF7CFC00),
+                unfocusedIndicatorColor = Color(0xFF7CFC00),
+                disabledIndicatorColor = Color.LightGray
+            )
 
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(
+            onClick = {
+                navController.navigate("login")
+            },
+            modifier = Modifier.fillMaxWidth()
+                .padding(10.dp)
+                .height(80.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF7CFC00),
+                contentColor = Color.Black
+            )
+        ) {
+            Text(
+                text="Login",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    textAlign = TextAlign.Center,
+                    fontFamily = FontFamily.SansSerif
+                )
+            )
+        }
+    }
 
 }
