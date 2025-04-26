7010c101b580bb3a196a5a8494422a686559e7ed
Henok Asaye <henokasaye77@gmail.com>
2025-04-26 20:06:19 +0300

    initiate the signup screen

--- app/src/main/java/com/example/community_repair_hub/Screen/SignupScreen.kt
+++ app/src/main/java/com/example/community_repair_hub/Screen/SignupScreen.kt
@@ -1,2 +1,13 @@
 package com.example.community_repair_hub.Screen
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+
+
+fun SignUpScreen(modifier: Modifier = Modifier, navController: NavHostController){
+    Text(text="Hello World")
+}
