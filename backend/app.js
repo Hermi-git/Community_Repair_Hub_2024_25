982fea37ca66befe49d97ed5a728167b56a881fc
Hermi-git <andargiehermela1@gmail.com>
2025-05-01 19:29:21 +0300

    add dependancies and add some functionality

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,19 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
-const app = express()
-app.use(cookieParser());
-app.use("/users", authRoutes)
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
+
+app.use("/users", authRoutes);
+
 export default app;
