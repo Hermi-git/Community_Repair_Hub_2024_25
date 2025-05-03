d932f538f0a4f1c9ff74729bb27a800e39112c51
Henok Asaye <henokasaye77@gmail.com>
2025-05-04 00:06:57 +0300

    remove the cookie part

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -9,12 +9,5 @@ export const generateWebToken = (res, id, role) => {
         { expiresIn: "1d" }
     );
 
-    res.cookie("auth_token", token, {
-        httpOnly: true,  
-        secure: process.env.NODE_ENV === "production", 
-        sameSite: "Strict",  
-        maxAge: 24 * 60 * 60 * 1000,  
-    });
-
     return token;  
 };
