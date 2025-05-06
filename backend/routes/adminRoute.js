cec58cbef1e24c0e77f006c6ece46fcd78414f77
Henok Asaye <henokasaye77@gmail.com>
2025-05-07 00:48:06 +0300

    modify the admin route

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -7,15 +7,12 @@ import {
     updateUser,
     getPendingRequests
 } from '../controllers/adminController.js';
-import { adminAuth } from '../middleware/authMiddleware.js';
-
 const router = express.Router();
-
-router.get('/users', adminAuth, listUsers);
-router.get('/pending-requests', adminAuth, getPendingRequests);
-router.post('/approve-repairteam', adminAuth, approveRepairTeam);
-router.post('/decline-repairteam', adminAuth, declineRepairTeam);
-router.delete('/delete-user', adminAuth, deleteUser);
-router.put('/update-user', adminAuth, updateUser);
+router.get('/users', listUsers);
+router.get('/pending-requests', getPendingRequests);
+router.post('/approve-repairteam', approveRepairTeam);
+router.post('/decline-repairteam', declineRepairTeam);
+router.delete('/delete-user', deleteUser);
+router.put('/update-user', updateUser);
 
 export default router;
