ecfcbeadb65e55eb4bbc765c3e661ca1078c9622
Henok Asaye <henokasaye77@gmail.com>
2025-05-09 02:46:50 +0300

    add the forgotpasswordResponse to handle the response

--- app/src/main/java/com/example/community_repair_hub/Data/network/Model/ForgotPasswordResponse.kt
+++ app/src/main/java/com/example/community_repair_hub/Data/network/Model/ForgotPasswordResponse.kt
@@ -1,3 +1,6 @@
-package com.example.community_repair_hub.Data.network.Model
+package com.example.community_repair_hub.data.network.model
 
-data class ForgotPasswordResponse()
+data class ForgotPasswordResponse(
+    val success: Boolean = false,
+    val message: String = ""
+)
