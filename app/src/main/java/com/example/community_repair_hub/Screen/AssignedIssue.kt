4a1a9c232dd6888c99a2d3248da0df7ae4a90e24
Henok Asaye <henokasaye77@gmail.com>
2025-05-09 02:56:34 +0300

    AssignedIssue

--- app/src/main/java/com/example/community_repair_hub/Screen/AssignedIssue.kt
+++ app/src/main/java/com/example/community_repair_hub/Screen/AssignedIssue.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -52,7 +52,7 @@ fun AssignedIssuesScreen(modifier: Modifier = Modifier, navController: NavHostCo
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            imageVector = Icons.Filled.ArrowBack,
                             contentDescription = "Back",
                             tint = Color.Black
                         )
