f508dcacc07704358ea891d04ddca1ff42fcbdde
Henok Asaye <henokasaye77@gmail.com>
2025-05-10 04:12:35 +0300

    fix bugs

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,19 @@ import cors from "cors";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import adminRoutes from "./routes/adminRoute.js";
-import adminRoutes from "./routes/adminRoute.js";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
