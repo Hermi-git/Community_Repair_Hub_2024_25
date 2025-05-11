3084f8e2150c571bc460fdf829db0592b5d96dc9
Hermi-git <andargiehermela1@gmail.com>
2025-05-11 11:37:34 +0300

    implement role based security for navigation

--- app/src/main/java/com/example/community_repair_hub/AppNavigation.kt
+++ app/src/main/java/com/example/community_repair_hub/AppNavigation.kt
@@ -6,49 +6,53 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.community_repair_hub.Screen.AuthScreen
-import com.example.community_repair_hub.Screen.HomeScreen
-import com.example.community_repair_hub.Screen.ReportIssueScreen
-import com.example.community_repair_hub.Screen.SignupScreen
-import com.example.community_repair_hub.Screen.ViewDetailScreen
-import com.example.community_repair_hub.Screen.LoginScreen
-//import com.example.community_repair_hub.Screen.RepairTeamHomeScreen
+import com.example.community_repair_hub.Screen.*
+import com.example.community_repair_hub.Utills.NavigationGuard
 import com.example.community_repair_hub.Utills.TokenManager
-import com.example.community_repair_hub.ViewModel.HomeViewModel
-import com.example.community_repair_hub.ViewModel.RepairDetailScreen
-//import com.example.community_repair_hub.ViewModel.RepairTeamHomeViewModel
+import com.example.community_repair_hub.ViewModel.*
 
 @Composable
-
-fun AppNavigation(modifier: Modifier=Modifier){
-    var navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "auth"){
-        composable("auth"){
-            AuthScreen(modifier,navController)
+fun AppNavigation(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    
+    NavHost(navController = navController, startDestination = "auth") {
+        composable("auth") {
+            AuthScreen(modifier, navController)
         }
-        composable("login"){
-            LoginScreen(modifier,navController)
+        composable("login") {
+            LoginScreen(modifier, navController)
         }
         composable("signup") {
             SignupScreen(modifier, navController)
         }
-        composable("home"){
-            HomeScreen(modifier,navController, viewModel = HomeViewModel())
+        composable("home") {
+            NavigationGuard(navController = navController, requiredRole = "citizen") {
+                HomeScreen(modifier, navController, viewModel = HomeViewModel())
+            }
         }
         composable("report") {
-            ReportIssueScreen(modifier, navController)
+            NavigationGuard(navController = navController, requiredRole = "citizen") {
+                ReportIssueScreen(modifier, navController)
+            }
         }
         composable("viewdetail") {
-            ViewDetailScreen(modifier, navController)
+            NavigationGuard(navController = navController, requiredRole = "citizen") {
+                ViewDetailScreen(modifier, navController)
+            }
         }
-//        composable("repairhome") {
-//            RepairTeamHomeScreen(
-//                modifier, navController, viewModel = RepairTeamHomeViewModel())
-//        }
+        composable("repairhome") {
+            NavigationGuard(navController = navController, requiredRole = "repairteam") {
+                RepairTeamHomeScreen(
+                    modifier = modifier,
+                    navController = navController,
+                    viewModel = RepairTeamHomeViewModel()
+                )
+            }
+        }
         composable("repairDetail") {
-            RepairDetailScreen(modifier, navController)
+            NavigationGuard(navController = navController, requiredRole = "repairteam") {
+                RepairDetailScreen(modifier, navController)
+            }
         }
-
     }
-
 }
